c57c40a4b7ae259b05d3f37d4f0551bddaad8e9e
eleagreugny <elea.greugny@insa-lyon.fr>
2017-03-01 10:44:58 +0100

    constructeur algoGen

--- algoGen.py
+++ algoGen.py
@@ -1,4 +1,11 @@
+import graph
+
 class AlgoGen:
 
-  def __init__(self):
-      
+  def __init__(self, N, n, e, Pm, Pc):
+    self.listGraph = []
+    for i in range(N):
+      G = Graph(i%3, i, n, e)
+      self.listGraph.append(G)
+    self.pm = Pm
+    self.pc = Pc
