432900c4710f4483059bcd14535003d70381a12c
eleagreugny <elea.greugny@insa-lyon.fr>
2017-03-01 11:53:41 +0100

    ajout methode coeffCluster

--- graphe.py
+++ graphe.py
@@ -1,3 +1,5 @@
+import networkx
+
 class Graphe :
 
   def __init__(self):
@@ -5,7 +7,7 @@ class Graphe :
 
 
 
-  def loiPuissance(G) : 
+  def loiPuissance(self, G) :
     k=[]
     pk=[]
     for i in range(self.size) :
@@ -16,8 +18,29 @@ class Graphe :
     if S>5 : 
       S=5
     r= 5-S
-    return r 
-    
+    return r
+
+  def coeffCluster(self):
+    loc_coeffCluster = []
+    for u in self.graph.nodes():
+      neighbors = u.neighbors()
+      n = 0
+      while neighbors:
+        v = neighbors[0]
+        del neighbors[0]
+        #ATTENTION ici on enleve un element de la liste sur laquelle on itere
+        #trouver un autre moyen
+        for w in neighbors:
+          if (v,w) in self.graph.edges() or (w,v) in self.graph.edges():
+            n += 1
+      C_u = 2*n/(self.graph.degree(u)*(self.graph.degree(u)-1))
+      loc_coeffCluster.append(C_u)
+    C = sum(loc_coeffCluster)/ self.size
+    S = abs(C - 1/log(self.size))/(1/log(self.size))
+    if S > 5 :
+      S = 5
+    r = 5 - S
+    return r
     
   def calculFitness() : 
     f=self.poidsPonderation[0]*loiPuissance() + self.poidsPonderation[1]*coeffCluster() + self.poidsPonderation[2]*diametreMoyen()
