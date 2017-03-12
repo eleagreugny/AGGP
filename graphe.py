0fc256d9c7a581650c13dcb0684a9620eba72ff6
eleagreugny <elea.greugny@insa-lyon.fr>
2017-03-12 15:30:07 +0100

    correction bugs coeffCluster()

--- graphe.py
+++ graphe.py
@@ -26,17 +26,18 @@ class Graphe :
     loc_coeffCluster = []
     degrees = []
     for u in self.graph.nodes():
-      neighbors = u.neighbors()
-      n = 0
-      while neighbors:
-        v = neighbors[0]
-        del neighbors[0]
-        for w in neighbors:
-          if (v,w) in self.graph.edges() or (w,v) in self.graph.edges():
-            n += 1
-      C_u = 2*n/(self.graph.degree(u)*(self.graph.degree(u)-1))
-      loc_coeffCluster.append(C_u)
-      degrees.append(self.graph.degree(u))
+      if self.graph.degree(u) > 1:
+        neighbors = self.graph.neighbors(u)
+        n = 0
+        while neighbors:
+          v = neighbors[0]
+          del neighbors[0]
+          for w in neighbors:
+            if (v,w) in self.graph.edges() or (w,v) in self.graph.edges():
+              n += 1
+        C_u = 2*n/float(self.graph.degree(u)*(self.graph.degree(u)-1))
+        loc_coeffCluster.append(C_u)
+        degrees.append(self.graph.degree(u))
     lr = stats.linregress(degrees,loc_coeffCluster)
     S = abs(lr[0] - 1.0)
     if S > 5 :
