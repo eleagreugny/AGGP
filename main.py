5d741d4e9bcac6278018d38697a73ef9325c444f
pbazelle <pauline.bazelle@gmail.com>
2017-03-22 10:03:50 +0100

    rien

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from algoGen import AlgoGen
 
 if __name__=="__main__":
-  algo = AlgoGen(10,500,400,0.001,0.00001)
+  algo = AlgoGen(100,200,99,0.1,0.01)#10,500,400,0.001,0.00001
   algo.simulation()
